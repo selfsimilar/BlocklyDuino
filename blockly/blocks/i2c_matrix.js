3210db694b67b595b2fcdacef73cc1191804c1cb
hiroyuki okada <ok.oakda.hiroyuki@gmail.com>
2015-08-22 21:13:22 +0900

    add blocks

--- blockly/blocks/i2c_matrix.js
+++ blockly/blocks/i2c_matrix.js
@@ -34,15 +34,17 @@ Blockly.Blocks.i2c_matrix.HUE = 225;
 
 Blockly.Blocks.i2c_mini_matrix_image = filepath.media+'/i2c_mini_matrix.jpg';
 
-Blockly.Blocks['i2c_mini_matrix_begin'] = {
+Blockly.Blocks['i2c_matrix_begin'] = {
   init: function() {
     this.setHelpUrl('');
     this.setColour(Blockly.Blocks.i2c_matrix.HUE);
     this.appendDummyInput()
-      .appendField("8x8 Matrix")
+      .appendField("I2C Matrix")
       .appendField(new Blockly.FieldImage(Blockly.Blocks.i2c_mini_matrix_image, 64, 64))
+      .appendField("Type")
+      .appendField(new Blockly.FieldDropdown(profile.default.i2c_matrix_type),"TYPE")
       .appendField("Address")
-      .appendField(new Blockly.FieldDropdown(profile.default.i2c_mini_matrix_address),"ADDRESS");
+      .appendField(new Blockly.FieldDropdown(profile.default.i2c_matrix_address),"ADDRESS");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setTooltip('');
@@ -96,6 +98,219 @@ Blockly.Blocks['i2c_matrix_print'] = {
       .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
       .appendField("text");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true,null);
+    this.setNextStatement(true,null);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['i2c_matrix_settextsize'] = {
+  init: function() {
+    this.setHelpUrl('');
+    this.setColour(Blockly.Blocks.i2c_matrix.HUE);
+    this.appendDummyInput()
+      .appendField("I2C Matrix")
+      .appendField(new Blockly.FieldImage(Blockly.Blocks.i2c_mini_matrix_image, 64, 64))
+      .appendField("setTextSize");
+    this.appendValueInput("SIZE")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("Size");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true,null);
+    this.setNextStatement(true,null);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['i2c_matrix_settextwrap'] = {
+  init: function() {
+    var BOOLEANS =
+        [[Blockly.Msg.LOGIC_BOOLEAN_TRUE, 'TRUE'],
+         [Blockly.Msg.LOGIC_BOOLEAN_FALSE, 'FALSE']];
+    this.setHelpUrl('');
+    this.setColour(Blockly.Blocks.i2c_matrix.HUE);
+    this.appendDummyInput()
+      .appendField("I2C Matrix")
+      .appendField(new Blockly.FieldImage(Blockly.Blocks.i2c_mini_matrix_image, 64, 64))
+      .appendField("setTextWrap");
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown(BOOLEANS), 'BOOL');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true,null);
+    this.setNextStatement(true,null);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['i2c_matrix_settextcolor'] = {
+  init: function() {
+    this.setHelpUrl('');
+    this.setColour(Blockly.Blocks.i2c_matrix.HUE);
+    this.appendDummyInput()
+      .appendField("I2C Matrix")
+      .appendField(new Blockly.FieldImage(Blockly.Blocks.i2c_mini_matrix_image, 64, 64))
+      .appendField("setTextColor");
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown(profile.default.i2c_matrix_color), 'COLOR');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true,null);
+    this.setNextStatement(true,null);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['i2c_matrix_drawpixel'] = {
+  init: function() {
+    this.setHelpUrl('');
+    this.setColour(Blockly.Blocks.i2c_matrix.HUE);
+    this.appendDummyInput()
+      .appendField("I2C Matrix")
+      .appendField(new Blockly.FieldImage(Blockly.Blocks.i2c_mini_matrix_image, 64, 64))
+      .appendField("drawPixel");
+    this.appendValueInput("X")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("x");
+    this.appendValueInput("Y")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("y");
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown(profile.default.i2c_matrix_color), 'COLOR');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true,null);
+    this.setNextStatement(true,null);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['i2c_matrix_drawcircle'] = {
+  init: function() {
+    this.setHelpUrl('');
+    this.setColour(Blockly.Blocks.i2c_matrix.HUE);
+    this.appendDummyInput()
+      .appendField("I2C Matrix")
+      .appendField(new Blockly.FieldImage(Blockly.Blocks.i2c_mini_matrix_image, 64, 64))
+      .appendField("drawCircle");
+    this.appendValueInput("X")
+      .setCheck("Number")
+      .appendField("x");
+    this.appendValueInput("Y")
+      .setCheck("Number")
+      .appendField("y");
+    this.appendValueInput("D")
+      .setCheck("Number")
+      .appendField("Diameter");
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown(profile.default.i2c_matrix_color), 'COLOR');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true,null);
+    this.setNextStatement(true,null);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['i2c_matrix_drawract'] = {
+  init: function() {
+    this.setHelpUrl('');
+    this.setColour(Blockly.Blocks.i2c_matrix.HUE);
+    this.appendDummyInput()
+      .appendField("I2C Matrix")
+      .appendField(new Blockly.FieldImage(Blockly.Blocks.i2c_mini_matrix_image, 64, 64))
+      .appendField("drawRact");
+    this.appendValueInput("X0")
+      .setCheck("Number")
+      .appendField("x0");
+    this.appendValueInput("Y0")
+      .setCheck("Number")
+      .appendField("y0");
+    this.appendValueInput("X1")
+      .setCheck("Number")
+      .appendField("x1");
+    this.appendValueInput("Y1")
+      .setCheck("Number")
+      .appendField("y1");
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown(profile.default.i2c_matrix_color), 'COLOR');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true,null);
+    this.setNextStatement(true,null);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['i2c_matrix_drawline'] = {
+  init: function() {
+    this.setHelpUrl('');
+    this.setColour(Blockly.Blocks.i2c_matrix.HUE);
+    this.appendDummyInput()
+      .appendField("I2C Matrix")
+      .appendField(new Blockly.FieldImage(Blockly.Blocks.i2c_mini_matrix_image, 64, 64))
+      .appendField("drawLine");
+    this.appendValueInput("X0")
+      .setCheck("Number")
+      .appendField("x0");
+    this.appendValueInput("Y0")
+      .setCheck("Number")
+      .appendField("y0");
+    this.appendValueInput("X1")
+      .setCheck("Number")
+      .appendField("x1");
+    this.appendValueInput("Y1")
+      .setCheck("Number")
+      .appendField("y1");
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown(profile.default.i2c_matrix_color), 'COLOR');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true,null);
+    this.setNextStatement(true,null);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['i2c_matrix_fillract'] = {
+  init: function() {
+    this.setHelpUrl('');
+    this.setColour(Blockly.Blocks.i2c_matrix.HUE);
+    this.appendDummyInput()
+      .appendField("I2C Matrix")
+      .appendField(new Blockly.FieldImage(Blockly.Blocks.i2c_mini_matrix_image, 64, 64))
+      .appendField("fillRact");
+    this.appendValueInput("X0")
+      .setCheck("Number")
+      .appendField("x0");
+    this.appendValueInput("Y0")
+      .setCheck("Number")
+      .appendField("y0");
+    this.appendValueInput("X1")
+      .setCheck("Number")
+      .appendField("x1");
+    this.appendValueInput("Y1")
+      .setCheck("Number")
+      .appendField("y1");
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown(profile.default.i2c_matrix_color), 'COLOR');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true,null);
+    this.setNextStatement(true,null);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['i2c_matrix_setrotation'] = {
+  init: function() {
+    this.setHelpUrl('');
+    this.setColour(Blockly.Blocks.i2c_matrix.HUE);
+    this.appendDummyInput()
+      .appendField("I2C Matrix")
+      .appendField(new Blockly.FieldImage(Blockly.Blocks.i2c_mini_matrix_image, 64, 64))
+      .appendField("setRotation");
+    this.appendValueInput("ANGLE")
+      .setCheck("Number")
+      .appendField("angle");
+    this.setInputsInline(true);
     this.setPreviousStatement(true,null);
     this.setNextStatement(true,null);
     this.setTooltip('');
@@ -110,6 +325,7 @@ Blockly.Blocks['i2c_matrix_writedisplay'] = {
       .appendField("I2C Matrix")
       .appendField(new Blockly.FieldImage(Blockly.Blocks.i2c_mini_matrix_image, 64, 64))
       .appendField("WriteDisplay");
+    this.setInputsInline(true);
     this.setPreviousStatement(true,null);
     this.setNextStatement(true,null);
     this.setTooltip('');
--- blockly/generators/arduino/i2c_matrix.js
+++ blockly/generators/arduino/i2c_matrix.js
@@ -2,13 +2,14 @@ goog.provide('Blockly.Arduino.i2c_matrix');
 
 goog.require('Blockly.Arduino');
 
-Blockly.Arduino.i2c_mini_matrix_begin = function() {
+Blockly.Arduino.i2c_matrix_begin = function() {
   var dropdown_address = this.getFieldValue('ADDRESS');
+  var type = this.getFieldValue('TYPE');
   Blockly.Arduino.definitions_['define_wire'] = '#include <Wire.h>\n';
   Blockly.Arduino.definitions_['define_adafruit_ledbackpack'] = '#include "Adafruit_LEDBackpack.h"\n';
   Blockly.Arduino.definitions_['define_adafruit_gfx'] = '#include "Adafruit_GFX.h"\n';
-  Blockly.Arduino.definitions_['define_8x8matrix'] = 'Adafruit_8x8matrix matrix = Adafruit_8x8matrix();\n';
-  Blockly.Arduino.setups_['setup_8x8matrix'+dropdown_address] = 'matrix.begin(' + dropdown_address + ');';
+  Blockly.Arduino.definitions_['define_' + type] = 'Adafruit_' + type + ' matrix = Adafruit_' + type + '();\n';
+  Blockly.Arduino.setups_['setup_' + type + '_' + dropdown_address] = 'matrix.begin(' + dropdown_address + ');';
   var code = "";
   return "";
 };
@@ -28,6 +29,77 @@ Blockly.Arduino.i2c_matrix_setcursor = function() {
 Blockly.Arduino.i2c_matrix_print = function() {
   var content = Blockly.Arduino.valueToCode(this, 'CONTENT', Blockly.Arduino.ORDER_ATOMIC) || ''
   var code = "matrix.print("+ content + ")";
+  return code;
+};
+
+Blockly.Arduino.i2c_matrix_settextsize = function() {
+  var size = Blockly.Arduino.valueToCode(this, 'SIZE', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var code = "matrix.textSize("+ size + ")";
+  return code;
+};
+
+Blockly.Arduino.i2c_matrix_settextcolor = function() {
+  var color = this.getFieldValue('COLOR');
+  var code = "matrix.setTextColor(" + color + ")";
+  return code;
+};
+
+Blockly.Arduino.i2c_matrix_settextwrap = function() {
+  var bool = (this.getFieldValue('BOOL') == 'TRUE') ? 'true' : 'false';
+  var code = "matrix.setTextWrap("+bool + ")";
+  return code;
+};
+
+Blockly.Arduino.i2c_matrix_drawpixel = function() {
+  var x = Blockly.Arduino.valueToCode(this, 'X', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var y = Blockly.Arduino.valueToCode(this, 'Y', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var color = this.getFieldValue('COLOR');
+  var code = "matrix.drawPixel(" + x + "," + y + "," + color + ")";
+  return code;
+};
+
+Blockly.Arduino.i2c_matrix_drawcircle = function() {
+  var x = Blockly.Arduino.valueToCode(this, 'X', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var y = Blockly.Arduino.valueToCode(this, 'Y', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var d = Blockly.Arduino.valueToCode(this, 'D', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var color = this.getFieldValue('COLOR');
+  var code = "matrix.drawCircle(" + x + "," + y + "," + d + "," + color + ")";
+  return code;
+};
+
+Blockly.Arduino.i2c_matrix_drawract = function() {
+  var x0 = Blockly.Arduino.valueToCode(this, 'X0', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var y0 = Blockly.Arduino.valueToCode(this, 'Y0', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var x1 = Blockly.Arduino.valueToCode(this, 'X1', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var y1 = Blockly.Arduino.valueToCode(this, 'Y1', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var color = this.getFieldValue('COLOR');
+  var code = "matrix.drawRact(" + x0 + "," + y0 + "," + x1 + "," + y1 + "," + color + ")";
+  return code;
+};
+
+Blockly.Arduino.i2c_matrix_drawline = function() {
+  var x0 = Blockly.Arduino.valueToCode(this, 'X0', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var y0 = Blockly.Arduino.valueToCode(this, 'Y0', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var x1 = Blockly.Arduino.valueToCode(this, 'X1', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var y1 = Blockly.Arduino.valueToCode(this, 'Y1', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var color = this.getFieldValue('COLOR');
+  var code = "matrix.drawLine(" + x0 + "," + y0 + "," + x1 + "," + y1 + "," + color + ")";
+  return code;
+};
+
+Blockly.Arduino.i2c_matrix_fillline = function() {
+  var x0 = Blockly.Arduino.valueToCode(this, 'X0', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var y0 = Blockly.Arduino.valueToCode(this, 'Y0', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var x1 = Blockly.Arduino.valueToCode(this, 'X1', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var y1 = Blockly.Arduino.valueToCode(this, 'Y1', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var color = this.getFieldValue('COLOR');
+  var code = "matrix.fillLine(" + x0 + "," + y0 + "," + x1 + "," + y1 + "," + color + ")";
+  return code;
+};
+
+Blockly.Arduino.i2c_matrix_setrotation = function() {
+  var angle = Blockly.Arduino.valueToCode(this, 'ANGLE', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var code = "matrix.setRotation(" + angle + ")";
   return code;
 };
 
