ee3d7c7700d141641b444d275245a74c421053ed
hiroyuki okada <ok.okada.hiroyuki@gmail.com>
2015-09-12 23:20:44 +0900

    add new m2s blocks

--- blockly/blocks/m2x.js
+++ blockly/blocks/m2x.js
@@ -120,3 +120,75 @@ Blockly.Blocks['m2x_custom_list_value'] = {
     this.setTooltip(Blockly.Msg.M2X_CUSTOM_LIST_VALUE_TOOLTIP);
   }
 };
+
+Blockly.Blocks['m2x_delete_values'] = {
+  init: function() {
+    this.setHelpUrl(Blockly.Msg.M2X_HELPURL);
+    this.setColour(Blockly.Blocks.m2x.HUE);
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldImage(Blockly.Blocks.m2x.image, 64, 64))
+      .appendField(Blockly.Msg.M2X_DELETE_VALUES_TITLE);
+    this.appendDummyInput()
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.M2X_DATE0_TITLE)
+      .appendField(new Blockly.FieldDate('2000-01-01'),'DATE0')
+      .appendField(new Blockly.FieldTextInput('00'),'HOUR0')
+      .appendField(":")
+      .appendField(new Blockly.FieldTextInput('00'),'MIN0')
+      .appendField(":")
+      .appendField(new Blockly.FieldTextInput('00'),'SEC0');
+    this.appendDummyInput()
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.M2X_DATE1_TITLE)
+      .appendField(new Blockly.FieldDate('2000-01-01'),'DATE1')
+      .appendField(new Blockly.FieldTextInput('01'),'HOUR1')
+      .appendField(":")
+      .appendField(new Blockly.FieldTextInput('00'),'MIN1')
+      .appendField(":")
+      .appendField(new Blockly.FieldTextInput('00'),'SEC1');
+    this.setOutput(true, 'Number');
+    this.setTooltip(Blockly.Msg.M2X_DELETE_VALUES_TOOLTIP);
+  }
+};
+
+Blockly.Blocks['m2x_update_location'] = {
+  init: function() {
+    this.setHelpUrl(Blockly.Msg.M2X_HELPURL);
+    this.setColour(Blockly.Blocks.m2x.HUE);
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldImage(Blockly.Blocks.m2x.image, 64, 64))
+      .appendField(Blockly.Msg.M2X_UPDATE_LOCATION_TITLE);
+    this.appendValueInput('LOCATION_NAME')
+      .setCheck('String')
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.M2X_LOCATION_NAME);
+    this.appendValueInput('LATITUDE')
+      .setCheck('Number')
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.LATITUDE);
+    this.appendValueInput('LONGITUDE')
+      .setCheck('Number')
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.LONGITUDE);
+    this.appendValueInput('ELEVATION')
+      .setCheck('Number')
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.ELEVATION);
+    this.setOutput(true, 'Number');
+    this.setTooltip(Blockly.Msg.M2X_UPDATE_VALUE_TOOLTIP);
+  }
+};
+
+Blockly.Blocks['m2x_read_location'] = {
+  init: function() {
+    this.setHelpUrl(Blockly.Msg.M2X_HELPURL);
+    this.setColour(Blockly.Blocks.m2x.HUE);
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldImage(Blockly.Blocks.m2x.image, 64, 64))
+      .appendField(Blockly.Msg.M2X_READ_LOCATION_TITLE);
+    this.setInputsInline(true);
+    this.setOutput(true, 'Number');
+    this.setTooltip(Blockly.Msg.M2X_READ_LOCATION_TOOLTIP);
+  }
+};
+
--- blockly/generators/arduino/m2x.js
+++ blockly/generators/arduino/m2x.js
@@ -28,7 +28,7 @@ goog.provide('Blockly.Arduino.m2x');
 
 goog.require('Blockly.Arduino');
 
-Blockly.Arduino.m2x_begin = function() {
+Blockly.Arduino.m2x_begin = function () {
   var version = this.getFieldValue('VERSION');
   var id = Blockly.Arduino.valueToCode(this, 'ID', Blockly.Arduino.ORDER_ATOMIC) || ''
   var stream = Blockly.Arduino.valueToCode(this, 'STREAM_NAME', Blockly.Arduino.ORDER_ATOMIC) || ''
@@ -42,22 +42,22 @@ Blockly.Arduino.m2x_begin = function() {
   Blockly.Arduino.definitions_['define_m2x_id'] = 'char m2xId[] = ' + id + ';';
   Blockly.Arduino.definitions_['define_m2x_stream_name'] = 'char m2xStream[] = ' + stream + ';';
   Blockly.Arduino.definitions_['define_m2x_key'] = 'char m2xKey[] = ' + key + ';';
-  Blockly.Arduino.definitions_['define_m2xstreamclient'] = 'M2XStreamClient m2xClient(&client, m2xKey);';
+  Blockly.Arduino.definitions_['define_m2xstreamclient'] = 'M2XStreamClient m2xClient(&client, m2xKey);\n';
 
   return "";
 };
 
-Blockly.Arduino.m2x_update_value = function() {
+Blockly.Arduino.m2x_update_value = function () {
   var value = Blockly.Arduino.valueToCode(this, 'VALUE', Blockly.Arduino.ORDER_ATOMIC) || '0'
 
-  var code =   'm2xClient.updateStreamValue(m2xId, m2xStream, ' + value + ')';
+  var code = 'm2xClient.updateStreamValue(m2xId, m2xStream, ' + value + ')';
   return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
 
-Blockly.Arduino.m2x_list_value = function() {
-  Blockly.Arduino.setups_['setup_serial_'+ profile.default.serial] = 'Serial.begin('+profile.default.serial+');\n';
+Blockly.Arduino.m2x_list_value = function () {
+  Blockly.Arduino.setups_['setup_serial_' + profile.default.serial] = 'Serial.begin(' + profile.default.serial + ');\n';
 
-  Blockly.Arduino.definitions_['define_on_data_point_found'] =  'void on_data_point_found(const char* at, const char* value, int index, void* context, int type) {\n' +
+  Blockly.Arduino.definitions_['define_on_data_point_found'] = 'void on_data_point_found(const char* at, const char* value, int index, void* context, int type) {\n' +
     '  Serial.print("Found a data point, index:");\n' +
     '  Serial.println(index);\n' +
     '  Serial.print("Type:");\n' +
@@ -72,20 +72,20 @@ Blockly.Arduino.m2x_list_value = function() {
   return code;
 };
 
-Blockly.Arduino.m2x_custom_list_value = function() {
+Blockly.Arduino.m2x_custom_list_value = function () {
   var date0 = this.getFieldValue('DATE0');
   var hour0 = this.getFieldValue('HOUR0');
   var min0 = this.getFieldValue('MIN0');
-  var sec0= this.getFieldValue('SEC0');
+  var sec0 = this.getFieldValue('SEC0');
   var date1 = this.getFieldValue('DATE1');
   var hour1 = this.getFieldValue('HOUR1');
   var min1 = this.getFieldValue('MIN1');
-  var sec1= this.getFieldValue('SEC1');
+  var sec1 = this.getFieldValue('SEC1');
 
 
-  Blockly.Arduino.setups_['setup_serial_'+ profile.default.serial] = 'Serial.begin('+profile.default.serial+');\n';
+  Blockly.Arduino.setups_['setup_serial_' + profile.default.serial] = 'Serial.begin(' + profile.default.serial + ');\n';
 
-  Blockly.Arduino.definitions_['define_on_data_point_found'] =  'void on_data_point_found(const char* at, const char* value, int index, void* context, int type) {\n' +
+  Blockly.Arduino.definitions_['define_on_data_point_found'] = 'void on_data_point_found(const char* at, const char* value, int index, void* context, int type) {\n' +
     '  Serial.print("Found a data point, index:");\n' +
     '  Serial.println(index);\n' +
     '  Serial.print("Type:");\n' +
@@ -96,8 +96,59 @@ Blockly.Arduino.m2x_custom_list_value = function() {
     '  Serial.println(value);\n' +
     '}\n';
 
-  var code =   'm2xClient.listStreamValues(m2xId, m2xStream, on_data_point_found, NULL, "start='+ date0 + 'T' + hour0 + ':' + min0 + ':' + sec0 + 'Z&end=' + date1 + 'T' + hour1 + ':' + min1 + ':' + sec1 + 'Z");\n';
+  var code = 'm2xClient.listStreamValues(m2xId, m2xStream, on_data_point_found, NULL, "start=' + date0 + 'T' + hour0 + ':' + min0 + ':' + sec0 + 'Z&end=' + date1 + 'T' + hour1 + ':' + min1 + ':' + sec1 + 'Z");\n';
   return code;
 };
 
+Blockly.Arduino.m2x_delete_values = function () {
+  var date0 = this.getFieldValue('DATE0');
+  var hour0 = this.getFieldValue('HOUR0');
+  var min0 = this.getFieldValue('MIN0');
+  var sec0 = this.getFieldValue('SEC0');
+  var date1 = this.getFieldValue('DATE1');
+  var hour1 = this.getFieldValue('HOUR1');
+  var min1 = this.getFieldValue('MIN1');
+  var sec1 = this.getFieldValue('SEC1');
 
+  var code = 'm2xClient.deleteValues(m2xId, m2xStream, "' + date0 + 'T' + hour0 + ':' + min0 + ':' + sec0 + 'Z","' + date1 + 'T' + hour1 + ':' + min1 + ':' + sec1 + 'Z")';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino.m2x_update_location = function () {
+  var name = Blockly.Arduino.valueToCode(this, 'LOCATION_NAME', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var lat = Blockly.Arduino.valueToCode(this, 'LATITUDE', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var lon = Blockly.Arduino.valueToCode(this, 'LONGITUDE', Blockly.Arduino.ORDER_ATOMIC) || '0'
+  var ele = Blockly.Arduino.valueToCode(this, 'ELEVATION', Blockly.Arduino.ORDER_ATOMIC) || '0'
+
+  var code = 'm2xClient.updateLocation(m2xId, ' + name + ', (double)' + lat + ', (double)' + lon + ', (double)' + ele + ')';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino.m2x_read_location = function () {
+  Blockly.Arduino.setups_['setup_serial_' + profile.default.serial] = 'Serial.begin(' + profile.default.serial + ');\n';
+
+  Blockly.Arduino.definitions_['define_on_location_found'] =
+    'void on_location_found(const char* name,\n' +
+    '                       double latitude,\n'+
+    '                       double longitude,\n'+
+    '                       double elevation,\n'+
+    '                       const char* timestamp,\n'+
+    '                       int index,\n'+
+    '                       void* context) {\n'+
+    '  Serial.print("Found a location, index:");\n'+
+    '  Serial.println(index);\n'+
+    '  Serial.print("Name: ");\n'+
+    '  Serial.println(name);\n'+
+    '  Serial.print("Latitude: ");\n'+
+    '  Serial.println(latitude);\n'+
+    '  Serial.print("Longitude: ");\n'+
+    '  Serial.println(longitude);\n'+
+    '  Serial.print("Elevation: ");\n'+
+    '  Serial.println(elevation);\n'+
+    '  Serial.print("Timestamp: ");\n'+
+    '  Serial.println(timestamp);\n'+
+    '}\n';
+
+  var code = 'm2xClient.readLocation(m2xId, on_location_found, NULL)';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
