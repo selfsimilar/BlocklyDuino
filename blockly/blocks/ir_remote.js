45dcb59e4874916a70df2905881b2254137d4dfd
gabrielkrell <gabrielkrell@gmail.com>
2017-03-05 15:37:16 -0600

    Add Button block
    Not sure if a None block makes more sense than using implicit null.

--- blockly/blocks/ir_remote.js
+++ blockly/blocks/ir_remote.js
@@ -8,13 +8,15 @@ goog.provide('Blockly.Arduino.ir_remote');
 
 goog.require('Blockly.Blocks');
 
+Blockly.Blocks.ir_remote.HUE = 215;
+
 Blockly.Blocks['ir_remote_get'] = {
   init: function() {
     this.appendValueInput("PIN")
         .setCheck("Number")
         .appendField("get button press on pin");
     this.setOutput(true, "String");
-    this.setColour(45);
+    this.setColour(Blockly.Blocks.ir_remote.HUE);
     this.setTooltip('');
     this.setHelpUrl('');
   }
@@ -22,11 +24,37 @@ Blockly.Blocks['ir_remote_get'] = {
 
 Blockly.Blocks['ir_remote_button'] = {
   init: function() {
+    var BUTTONS =
+      [[Blockly.Msg.IR_REMOTE_BUTTON_POWER, 'POWER'],
+       [Blockly.Msg.IR_REMOTE_BUTTON_A, 'A'],
+       [Blockly.Msg.IR_REMOTE_BUTTON_B, 'B'],
+       [Blockly.Msg.IR_REMOTE_BUTTON_C, 'C'],
+       [Blockly.Msg.IR_REMOTE_BUTTON_UP, 'UP'],
+       [Blockly.Msg.IR_REMOTE_BUTTON_DOWN, 'DOWN'],
+       [Blockly.Msg.IR_REMOTE_BUTTON_LEFT, 'LEFT'],
+       [Blockly.Msg.IR_REMOTE_BUTTON_RIGHT, 'RIGHT'],
+       [Blockly.Msg.IR_REMOTE_BUTTON_ENTER, 'ENTER']]
     this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown([["Up","UP"], ["Down","DOWN"], ["Etc","ET CETERA"]]), "BUTTON");
-    this.setOutput(true, "remote_button");
-    this.setColour(230);
-    this.setTooltip('');
-    this.setHelpUrl('');
+        .appendField(new Blockly.FieldDropdown(BUTTONS), 'BUTTON')
+        .appendField(' ' + Blockly.Msg.IR_REMOTE_BUTTON_BUTTON);
+    this.setOutput(true, "remote_button"); // TODO: investigate proper location
+    this.setColour(Blockly.Blocks.ir_remote.HUE);
+    this.setHelpUrl(Blockly.Msg.IR_REMOTE_BUTTON_HELPURL);
+    var thisBlock = this;
+    this.setTooltip(function() {
+      var currButton = thisBlock.getFieldValue('BUTTON');
+      var TOOLTIPS =  {
+        'POWER': Blockly.Msg.IR_REMOTE_TOOLTIP_BUTTON_POWER,
+        'A': Blockly.Msg.IR_REMOTE_TOOLTIP_BUTTON_A,
+        'B': Blockly.Msg.IR_REMOTE_TOOLTIP_BUTTON_B,
+        'C': Blockly.Msg.IR_REMOTE_TOOLTIP_BUTTON_C,
+        'UP': Blockly.Msg.IR_REMOTE_TOOLTIP_BUTTON_UP,
+        'DOWN': Blockly.Msg.IR_REMOTE_TOOLTIP_BUTTON_DOWN,
+        'LEFT': Blockly.Msg.IR_REMOTE_TOOLTIP_BUTTON_LEFT,
+        'RIGHT': Blockly.Msg.IR_REMOTE_TOOLTIP_BUTTON_RIGHT,
+        'ENTER': Blockly.Msg.IR_REMOTE_TOOLTIP_BUTTON_ENTER
+      };
+      return TOOLTIPS[currButton];
+    });
   }
 };
