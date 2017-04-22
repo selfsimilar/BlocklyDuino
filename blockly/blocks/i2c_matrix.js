20c372330da2b05f1e270e7bf34cd32f76373417
gabrielkrell <gabrielkrell@gmail.com>
2017-04-21 21:13:11 -0500

    Separate I2C matrices; revert 0b8ef9
    Remove HT1632 stuff from the I2C matrix blocks

--- blockly/blocks/i2c_matrix.js
+++ blockly/blocks/i2c_matrix.js
@@ -74,13 +74,7 @@ Blockly.Blocks['i2c_matrix_begin'] = {
       .appendField(new Blockly.FieldImage(Blockly.Blocks.mini_matrix_image, 64, 64))
       .appendField(Blockly.Msg.I2C_MATRIX_BEGIN_TITLE)
       .appendField(Blockly.Msg.I2C_MATRIX_TYPE)
-      .appendField(new Blockly.FieldDropdown(
-        profile.default.i2c_matrix_type, function(option) {
-          var isHT1632 = (option == 'HT1632_16x24');
-          this.sourceBlock_.updateShape_(isHT1632);
-        }),
-        "TYPE")
-    this.appendDummyInput("i2c_address")
+      .appendField(new Blockly.FieldDropdown(profile.default.i2c_matrix_type),"TYPE")
       .appendField(Blockly.Msg.I2C_MATRIX_ADDRESS)
       .appendField(new Blockly.FieldDropdown(profile.default.led_backpack_address),"ADDRESS");
     this.setPreviousStatement(true, null);
@@ -93,65 +87,7 @@ Blockly.Blocks['i2c_matrix_begin'] = {
       // Block has been deleted.
       return;
     }
-    if (this.getFieldValue('TYPE') == 'HT1632_16x24') {
-      this.setWarningText("Need to install the Adafruit-GFX library and the Adafruit-HT1632 library.")
-    } else {
-      this.setWarningText(Blockly.Msg.I2C_MATRIX_BEGIN_WARNING);
-    }
-  },
-    /**
-   * Create XML to represent whether the HT1632 pin inputs should be present.
-   * @return {Element} XML storage element.
-   * @this Blockly.Block
-   */
-  mutationToDom: function() {
-    var container = document.createElement('mutation');
-    var isHT1632 = (this.getFieldValue('TYPE') == 'HT1632_16x24');
-    container.setAttribute('is_ht1632', isHT1632);
-    return container;
-  },
-  /**
-   * Parse XML to restore the HT1632 input pins or i2c address select.
-   * @param {!Element} xmlElement XML storage element.
-   * @this Blockly.Block
-   */
-  domToMutation: function(xmlElement) {
-    var isHT1632 = (xmlElement.getAttribute('is_ht1632') == 'true');
-    this.updateShape_(isHT1632);
-  },
-  /**
-   * Modify this block to have(or not) inputs for HT1632 pins instead of an i2c address.
-   * @param {boolean} isHT1632 True if this block starts an HT1632 matrix.
-   * @private
-   * @this Blockly.Block
-   */
-  updateShape_: function(isHT1632) {
-    // Add or remove a Value Input.
-    var HT1632inputExists = this.getInput('HT1632_inputs');
-    var addressInputExists = this.getInput('i2c_address');
-    if (isHT1632) {
-      if (!HT1632inputExists) {
-        this.appendDummyInput("HT1632_inputs")
-        .appendField("Data pin:")
-        .appendField(new Blockly.FieldDropdown(profile.default.digital), "HT_DATA")
-        .appendField("WR pin:")
-        .appendField(new Blockly.FieldDropdown(profile.default.digital), "HT_WR")
-        .appendField("CS pin:")
-        .appendField(new Blockly.FieldDropdown(profile.default.digital), "HT_CS");
-      }
-      if (addressInputExists) {
-        this.removeInput("i2c_address")
-      }
-    } else {
-      if (HT1632inputExists) {
-        this.removeInput("HT1632_inputs");
-      }
-      if (!addressInputExists) {
-        this.appendDummyInput("i2c_address")
-          .appendField(Blockly.Msg.I2C_MATRIX_ADDRESS)
-          .appendField(new Blockly.FieldDropdown(profile.default.led_backpack_address),"ADDRESS");
-      }
-    }
+    this.setWarningText(Blockly.Msg.I2C_MATRIX_BEGIN_WARNING);
   }
 };
 
