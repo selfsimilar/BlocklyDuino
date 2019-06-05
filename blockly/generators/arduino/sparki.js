6bb6d5a6679bfdf89ffd22f665455fc19fa415b7
kateew <katewilliams2020@u.northwestern.edu>
2019-06-05 12:36:22 -0500

    code produced for sparki blocks defined

--- blockly/generators/arduino/sparki.js
+++ blockly/generators/arduino/sparki.js
@@ -4,80 +4,88 @@ goog.provide('Blockly.Arduino.sparki');
 
 goog.require('Blockly.Arduino');
 
-Blockly.Arduino.sparki_move_forward = function() {
-  var value_forwarddist = Blockly.Arduino.valueToCode(block, 'forwardDist', Blockly.Arduino.ORDER_ATOMIC);
+
+Blockly.Arduino['sparki_move_forward'] = function() {
+  var value_forwarddist = this.getFieldValue('forwardDist');
   var code = 'sparki.moveForward(' + value_forwarddist + ');\n';
+  console.log("debug")
   return code;
 };
 
-Blockly.Arduino.sparki_move_backward = function() {
-  var value_backwarddist = Blockly.Arduino.valueToCode(block, 'backwardDist', Blockly.Arduino.ORDER_ATOMIC);
+Blockly.Arduino['sparki_move_backward'] = function() {
+  var value_backwarddist = this.getFieldValue('backwardDist');
   var code = 'sparki.moveBackward(' + value_backwarddist + ');\n';
   return code;
 };
 
-Blockly.Arduino.sparki_move_left = function() {
-  var value_leftangle = Blockly.Arduino.valueToCode(block, 'leftAngle', Blockly.Arduino.ORDER_ATOMIC);
+Blockly.Arduino['sparki_move_left'] = function() {
+  var value_leftangle = this.getFieldValue('leftAngle');
   var code = 'sparki.moveLeft(' + value_leftangle + ');\n';
   return code;
 }
 
-Blockly.Arduino.sparki_move_right = function() {
-  var value_rightangle = Blockly.Arduino.valueToCode(block, 'rightAngle', Blockly.Arduino.ORDER_ATOMIC);
+Blockly.Arduino['sparki_move_right'] = function() {
+  var value_rightangle = this.getFieldValue('rightAngle');
   var code = 'sparki.moveRight(' + value_rightangle + ');\n';
   return code;
 };
 
-Blockly.Arduino.sparki_opengripper = function() {
-  var value_opengripperdist = Blockly.Arduino.valueToCode(block, 'openGripperDist', Blockly.Arduino.ORDER_ATOMIC);
+Blockly.Arduino['sparki_opengripper'] = function() {
+  var value_opengripperdist = this.getFieldValue('openGripperDist');
   var code = 'sparki.gripperOpen(' + value_opengripperdist + ');\n';
   return code;
 };
 
-Blockly.Arduino.sparki_closegripper = function() {
-  var value_closegripperdist = Blockly.Arduino.valueToCode(block, 'closeGripperDist', Blockly.Arduino.ORDER_ATOMIC);
+Blockly.Arduino['sparki_closegripper'] = function() {
+  var value_closegripperdist = this.getFieldValue('closeGripperDist');
   var code = 'sparki.gripperClose(' + value_closegripperdist + ');\n';
   return code;
 };
 
-Blockly.Arduino.sparki_stop_movement = function() {
+Blockly.Arduino['sparki_stop_movement'] = function() {
   var code = 'sparki.moveStop();\n';
   return code;
 };
 
-Blockly.Arduino.sparki_stopgripper = function() {
+Blockly.Arduino['sparki_stopgripper'] = function() {
   var code = 'sparki.gripperStop();\n';
   return code;
 };
 
 Blockly.Arduino.sparki_bark = function() {
-  var value_frequency = Blockly.Arduino.valueToCode(block, 'frequency', Blockly.Arduino.ORDER_ATOMIC);
-  var value_time = Blockly.Arduino.valueToCode(block, 'Time', Blockly.Arduino.ORDER_ATOMIC);
+  var value_frequency = this.getFieldValue('frequency');
+  var value_time = this.getFieldValue('Time');
   var code = 'sparki.beep(' + value_frequency + ',' + value_time +');\n';
   return code;
 };
 
-Blockly.Arduino.sparki_ultrasonicdistance = function() {
-  var code = 'sparki.ping();\n';
-  return [code, Blockly.Arduino.ORDER_NONE];
+Blockly.Arduino['sparki_ultrasonicdistance'] = function() {
+  var code = 'sparki.ping()';
+  return code;
 };
+
+Blockly.Arduino['sparki_lose'] = function() {
+  var code = 'sparki.moveRight(random(0, 360));\n';
+  return code;
+}
 
 Blockly.Arduino.sparki_leftlight = function() {
-  var code = 'sparki.lightLeft();\n';
-  return [code, Blockly.Arduino.ORDER_NONE];
-};
-
-Blockly.Arduino.sparki_rightlight = function() {
-  var code = 'sparki.lightRight();\n';
-  return [code, Blockly.Arduino.ORDER_NONE];
-};
-
-Blockly.Arduino.sparki_centerlight = function() {
-  var code = 'sparki.lightCenter();\n';
+  var code = 'sparki.lightLeft()';
   return code;
 };
 
-Blockly.Arduino.include_sparki_library = function() {
-  var code = '#include <sparki.h>;\n';
+Blockly.Arduino['sparki_rightlight'] = function() {
+  var code = 'sparki.lightRight()';
   return code;
+};
+
+Blockly.Arduino['sparki_centerlight'] = function() {
+  var code = 'sparki.lightCenter()';
+  return code;
+};
+
+Blockly.Arduino['include_sparki_library'] = function() {
+  Blockly.Arduino.definitions_['define_sparki'] = '#include <sparki.h>;\n';
+  var code = '';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
